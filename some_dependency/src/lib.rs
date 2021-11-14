4aab38be9ca45ff38f9ea581a80171cce7a3f323
Frank Steffahn <frank.steffahn@stu.uni-kiel.de>
2021-11-14 20:23:19 +0100

    Remove unnecessary `Debug` implementation

--- some_dependency/src/lib.rs
+++ some_dependency/src/lib.rs
@@ -4,12 +4,7 @@ pub fn useful_api() {}
 mod dont_look_at_this {
     mod really_nobody_should_care_about_this {
         pub struct Pwned;
-        use std::{cell, fmt, ops};
-        impl fmt::Debug for Pwned {
-            fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-                formatter.write_str("[21, 42, 63]")
-            }
-        }
+        use std::{cell, ops};
         impl AsRef<Pwned> for cell::Ref<'_, Box<[u8]>> {
             fn as_ref(&self) -> &Pwned {
                 &Pwned
